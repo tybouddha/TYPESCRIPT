f7eeea6519ec37d276b752cf80e1b83bb05712cc
Vi Dev <vi@videv.fr>
2024-09-28 08:04:31 +0200

    ♻️ R01 : refacto > Migrate from CRA to vitejs > fix BrowserRouter bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  // <StrictMode>
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>
-  // </StrictMode>
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>
 );
