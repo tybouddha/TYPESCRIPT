e02ebb776136fce819fad82f4366abab8b6549ad
Vi Dev <vi@videv.fr>
2024-09-28 07:24:35 +0200

    ♻️ R01 : refacto > Migrate from CRA to vitejs > add main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  // <StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </StrictMode>,
-)
+  </ThemeProvider>
+  // </StrictMode>
+);
