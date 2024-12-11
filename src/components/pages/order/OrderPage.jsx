43c8bb785e48b3c4f88948bce8c243c199ac923a
Vi Dev <vi@videv.fr>
2024-12-11 21:34:40 +0100

    ♻️ F17-refacto-useContext-avance : replace creation and installation of Context (advanced)

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,74 +1,22 @@
-import { useEffect, useRef, useState } from "react"
 import styled from "styled-components"
 import { theme } from "../../../theme"
 import Main from "./Main/Main"
 import Navbar from "./Navbar/Navbar"
-import OrderContext from "../../../context/OrderContext"
-import { EMPTY_PRODUCT } from "../../../enums/product"
-import { useMenu } from "../../../hooks/useMenu"
-import { useBasket } from "../../../hooks/useBasket"
-import { findObjectById } from "../../../utils/array"
-import { useParams } from "react-router-dom"
-import { initialiseUserSession } from "./helpers/initialiseUserSession"
+import { OrderContextProvider } from "../../../context/OrderContext"
 
 export default function OrderPage() {
   // state
-  const [isModeAdmin, setIsModeAdmin] = useState(false)
-  const [isCollapsed, setIsCollapsed] = useState(false)
-  const [currentTabSelected, setCurrentTabSelected] = useState("add")
-  const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT)
-  const [productSelected, setProductSelected] = useState(EMPTY_PRODUCT)
-  const titleEditRef = useRef()
-  const { menu, setMenu, handleAdd, handleDelete, handleEdit, resetMenu } = useMenu()
-  const { basket, setBasket, handleAddToBasket, handleDeleteBasketProduct } = useBasket()
-  const { username } = useParams()
-
-  const handleProductSelected = async (idProductClicked) => {
-    const productClickedOn = findObjectById(idProductClicked, menu)
-    await setIsCollapsed(false)
-    await setCurrentTabSelected("edit")
-    await setProductSelected(productClickedOn)
-    titleEditRef.current.focus()
-  }
-
-  useEffect(() => {
-    initialiseUserSession(username, setMenu, setBasket)
-  }, [])
-
-  const orderContextValue = {
-    username,
-    isModeAdmin,
-    setIsModeAdmin,
-    isCollapsed,
-    setIsCollapsed,
-    currentTabSelected,
-    setCurrentTabSelected,
-    menu,
-    handleAdd,
-    handleDelete,
-    resetMenu,
-    newProduct,
-    setNewProduct,
-    productSelected,
-    setProductSelected,
-    handleEdit,
-    titleEditRef,
-    basket,
-    handleAddToBasket,
-    handleDeleteBasketProduct,
-    handleProductSelected,
-  }
 
   //affichage (render)
   return (
-    <OrderContext.Provider value={orderContextValue}>
+    <OrderContextProvider>
       <OrderPageStyled>
         <div className="container">
           <Navbar />
           <Main />
         </div>
       </OrderPageStyled>
-    </OrderContext.Provider>
+    </OrderContextProvider>
   )
 }
 
--- src/context/OrderContext.jsx
+++ src/context/OrderContext.jsx
@@ -1,6 +1,13 @@
-import { createContext } from "react"
+import { createContext, useEffect, useRef, useState } from "react"
+import { useMenu } from "../hooks/useMenu"
+import { useBasket } from "../hooks/useBasket"
+import { findObjectById } from "../utils/array"
+import { EMPTY_PRODUCT } from "../enums/product"
+import { useParams } from "react-router-dom"
+import { initialiseUserSession } from "../components/pages/order/helpers/initialiseUserSession"
 
-export default createContext({
+// 1. Création du context
+const OrderContext = createContext({
   username: "",
   isModeAdmin: false,
   setIsModeAdmin: () => {},
@@ -30,3 +37,59 @@ export default createContext({
   handleAddToBasket: () => {},
   handleDeleteBasketProduct: () => {},
 })
+
+export default OrderContext
+
+// 2. Installation du context
+
+export const OrderContextProvider = ({ children }) => {
+  const [isModeAdmin, setIsModeAdmin] = useState(false)
+  const [isCollapsed, setIsCollapsed] = useState(false)
+  const [currentTabSelected, setCurrentTabSelected] = useState("add")
+  const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT)
+  const [productSelected, setProductSelected] = useState(EMPTY_PRODUCT)
+  const titleEditRef = useRef()
+  const { menu, setMenu, handleAdd, handleDelete, handleEdit, resetMenu } = useMenu()
+  const { basket, setBasket, handleAddToBasket, handleDeleteBasketProduct } = useBasket()
+  const { username } = useParams()
+
+  const handleProductSelected = async (idProductClicked) => {
+    const productClickedOn = findObjectById(idProductClicked, menu)
+    await setIsCollapsed(false)
+    await setCurrentTabSelected("edit")
+    await setProductSelected(productClickedOn)
+    titleEditRef.current.focus()
+  }
+
+  useEffect(() => {
+    initialiseUserSession(username, setMenu, setBasket)
+  }, [])
+
+  const orderContextValue = {
+    username,
+    isModeAdmin,
+    setIsModeAdmin,
+    isCollapsed,
+    setIsCollapsed,
+    currentTabSelected,
+    setCurrentTabSelected,
+    menu,
+    handleAdd,
+    handleDelete,
+    resetMenu,
+    newProduct,
+    setNewProduct,
+    productSelected,
+    setProductSelected,
+    handleEdit,
+    titleEditRef,
+    basket,
+    handleAddToBasket,
+    handleDeleteBasketProduct,
+    handleProductSelected,
+  }
+
+  return <OrderContext.Provider value={orderContextValue}>{children}</OrderContext.Provider>
+}
+
+// 3. Consommation du context
