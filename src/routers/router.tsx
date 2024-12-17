d6a2718d7ddaa9e1eca566f286329beaf3c17c5c
Vi Dev <vi@videv.fr>
2024-12-17 22:02:16 +0100

    TSE-10: add router.tsx (finished)

--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -1,8 +1,7 @@
 import { createBrowserRouter } from "react-router-dom"
-import LoginPage from "../components/pages/login/LoginPage"
-//@ts-ignore
-import OrderPage from "../components/pages/order/OrderPage"
-import ErrorPage from "../components/pages/error/ErrorPage"
+import LoginPage from "@/components/pages/login/LoginPage"
+import OrderPage from "@/components/pages/order/OrderPage"
+import ErrorPage from "@/components/pages/error/ErrorPage"
 
 
 const routes = [
