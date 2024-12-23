65e514f2c7d63d04875b6161d020ecd2d7f4e459
Vi Dev <vi@videv.fr>
2024-12-23 21:32:56 +0100

    TSE-15: add main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,10 @@ import { RouterProvider } from "react-router-dom"
 import { router } from "./routers/router"
 import { OrderContextProvider } from "./context/OrderContext"
 
-createRoot(document.getElementById("root")).render(
+const rootElement = document.getElementById("root")
+if (!rootElement) throw new Error('Element with id "root" not found')
+
+createRoot(rootElement).render(
   <ThemeProvider theme={theme}>
     <OrderContextProvider>
       <RouterProvider router={router} />
