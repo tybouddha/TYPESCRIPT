fb9aa905e339ca44956243c44031eaf7de5e8a3d
Tybouddha <bruneecedric@gmail.com>
2025-09-25 12:02:32 +0200

    🏷️ TSE-23-CATEGORIES: useCategories > Add types on useCategories.ts

--- src/api/categories.ts
+++ src/api/categories.ts
@@ -1,21 +1,24 @@
-import { doc, getDoc, updateDoc } from "firebase/firestore"
-import { db } from "./firebase-config"
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { db } from "./firebase-config";
 
-export const getCategories = async (idUser) => {
+export const getCategories = async (idUser: string) => {
   //const docRef = doc(CHEMIN)
-  const docRef = doc(db, "users", idUser)
+  const docRef = doc(db, "users", idUser);
 
-  const docSnapshot = await getDoc(docRef)
+  const docSnapshot = await getDoc(docRef);
   if (docSnapshot.exists()) {
-    const { categories } = docSnapshot.data()
-    return categories as any
+    const { categories } = docSnapshot.data();
+    return categories as any;
   }
-}
+};
 
-export const updateCategoriesInDB = async (userId: any, categoriesUpdated: any) => {
-  const docRef = doc(db, "users", userId)
+export const updateCategoriesInDB = async (
+  userId: string,
+  categoriesUpdated: any
+) => {
+  const docRef = doc(db, "users", userId);
 
   await updateDoc(docRef, {
     categories: categoriesUpdated,
-  })
-}
+  });
+};
