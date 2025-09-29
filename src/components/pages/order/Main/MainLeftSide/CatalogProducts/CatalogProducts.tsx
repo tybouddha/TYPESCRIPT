670a3e2c802e33933148c43169a3474994dc6bf4
Tybouddha <bruneecedric@gmail.com>
2025-09-29 13:29:10 +0200

    🏷️ TSE-24-FILTERS: MainLeftSide > CatalogProducts > Add chek if activeCategory is undefined

--- src/components/pages/order/Main/MainLeftSide/CatalogProducts/CatalogProducts.tsx
+++ src/components/pages/order/Main/MainLeftSide/CatalogProducts/CatalogProducts.tsx
@@ -1,23 +1,23 @@
-import styled from "styled-components"
-import { useOrderContext } from "@/context/OrderContext"
-import { theme } from "@/theme/theme"
-import { formatPrice } from "@/utils/maths"
-import Card from "@/components/reusable-ui/Card"
-import EmptyCatalogProductsAdmin from "./EmptyCatalogProductsAdmin"
-import EmptyCatalogProductsClient from "./EmptyCatalogProductsClient"
-import { checkIfProductIsClicked, getProductsToDisplay } from "./helper"
+import styled from "styled-components";
+import { useOrderContext } from "@/context/OrderContext";
+import { theme } from "@/theme/theme";
+import { formatPrice } from "@/utils/maths";
+import Card from "@/components/reusable-ui/Card";
+import EmptyCatalogProductsAdmin from "./EmptyCatalogProductsAdmin";
+import EmptyCatalogProductsClient from "./EmptyCatalogProductsClient";
+import { checkIfProductIsClicked, getProductsToDisplay } from "./helper";
 import {
   EMPTY_PRODUCT,
   IMAGE_COMING_SOON,
   IMAGE_NO_STOCK,
-} from "@/constants/product"
-import { getCategoryActive, isEmpty } from "@/utils/array"
-import LoadingMessage from "./LoadingMessage"
-import { CSSTransition, TransitionGroup } from "react-transition-group"
-import { menuAnimation } from "@/theme/animations"
-import { convertStringToBoolean } from "@/utils/string"
-import RibbonAnimated, { ribbonAnimation } from "./RibbonAnimated"
-import { useParams } from "react-router-dom"
+} from "@/constants/product";
+import { getCategoryActive, isEmpty } from "@/utils/array";
+import LoadingMessage from "./LoadingMessage";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { menuAnimation } from "@/theme/animations";
+import { convertStringToBoolean } from "@/utils/string";
+import RibbonAnimated, { ribbonAnimation } from "./RibbonAnimated";
+import { useParams } from "react-router-dom";
 
 export default function CatalogProducts() {
   const {
@@ -32,65 +32,92 @@ export default function CatalogProducts() {
     handleProductSelected,
     categories,
     categoryAll,
-  } = useOrderContext()
+  } = useOrderContext();
   // state
 
-  const { username } = useParams()
+  const { username } = useParams();
 
   // comportements (gestionnaires d'événement ou "event handlers")
-  const handleCardDelete = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, idProductToDelete: string) => {
-    event.stopPropagation()
-    if (!username) return
-    handleDelete(idProductToDelete, username)
-    handleDeleteBasketProduct(idProductToDelete, username)
-    idProductToDelete === productSelected.id && setProductSelected(EMPTY_PRODUCT)
-  }
+  const handleCardDelete = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    idProductToDelete: string
+  ) => {
+    event.stopPropagation();
+    if (!username) return;
+    handleDelete(idProductToDelete, username);
+    handleDeleteBasketProduct(idProductToDelete, username);
+    idProductToDelete === productSelected.id &&
+      setProductSelected(EMPTY_PRODUCT);
+  };
 
-  const handleAddButton = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, idProductToAdd: string) => {
-    event.stopPropagation()
-    username && handleAddToBasket(idProductToAdd, username)
-  }
+  const handleAddButton = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    idProductToAdd: string
+  ) => {
+    event.stopPropagation();
+    username && handleAddToBasket(idProductToAdd, username);
+  };
 
-  let cardContainerClassName = isModeAdmin ? "card-container is-hoverable" : "card-container"
+  let cardContainerClassName = isModeAdmin
+    ? "card-container is-hoverable"
+    : "card-container";
 
   // affichage
-  if (menu === undefined) return <LoadingMessage />
+  if (menu === undefined) return <LoadingMessage />;
 
   if (isEmpty(menu)) {
-    if (!isModeAdmin) return <EmptyCatalogProductsClient />
-    if (username) return <EmptyCatalogProductsAdmin onReset={() => resetMenu(username)} />
+    if (!isModeAdmin) return <EmptyCatalogProductsClient />;
+    if (username)
+      return <EmptyCatalogProductsAdmin onReset={() => resetMenu(username)} />;
   }
 
-  const activeCategory = getCategoryActive(categories)
-  const productsToDisplay = getProductsToDisplay(categoryAll, menu, activeCategory)
+  const activeCategory = getCategoryActive(categories);
+  if (!activeCategory) return;
+  const productsToDisplay = getProductsToDisplay(
+    categoryAll,
+    menu,
+    activeCategory
+  );
 
   return (
     <TransitionGroup component={CatalogProductsStyled} className="menu">
-      {productsToDisplay.map(({ id, title, imageSource, price, isAvailable, isPublicised, categories }) => {
-        return (
-          <CSSTransition classNames={"menu-animation"} key={id} timeout={300}>
-            <div className={cardContainerClassName}>
-              {convertStringToBoolean(isPublicised) && <RibbonAnimated />}
-              <Card
-                title={title}
-                imageSource={imageSource ? imageSource : IMAGE_COMING_SOON}
-                leftDescription={formatPrice(price)}
-                hasDeleteButton={isModeAdmin}
-                onDelete={(event) => handleCardDelete(event, id)}
-                onClick={() => handleProductSelected(id)}
-                isHoverable={isModeAdmin}
-                isSelected={checkIfProductIsClicked(id, productSelected.id)}
-                onAdd={(event) => handleAddButton(event, id)}
-                overlapImageSource={IMAGE_NO_STOCK}
-                isOverlapImageVisible={convertStringToBoolean(isAvailable) === false}
-                categories={categories}
-              />
-            </div>
-          </CSSTransition>
-        )
-      })}
+      {productsToDisplay.map(
+        ({
+          id,
+          title,
+          imageSource,
+          price,
+          isAvailable,
+          isPublicised,
+          categories,
+        }) => {
+          return (
+            <CSSTransition classNames={"menu-animation"} key={id} timeout={300}>
+              <div className={cardContainerClassName}>
+                {convertStringToBoolean(isPublicised) && <RibbonAnimated />}
+                <Card
+                  title={title}
+                  imageSource={imageSource ? imageSource : IMAGE_COMING_SOON}
+                  leftDescription={formatPrice(price)}
+                  hasDeleteButton={isModeAdmin}
+                  onDelete={(event) => handleCardDelete(event, id)}
+                  onClick={() => handleProductSelected(id)}
+                  isHoverable={isModeAdmin}
+                  isSelected={checkIfProductIsClicked(id, productSelected.id)}
+                  onAdd={(event) => handleAddButton(event, id)}
+                  overlapImageSource={IMAGE_NO_STOCK}
+                  isOverlapImageVisible={
+                    convertStringToBoolean(isAvailable) === false
+                  }
+                  categories={categories}
+                />
+              </div>
+            </CSSTransition>
+          );
+        }
+      )}
     </TransitionGroup>
-  )
+  );
 }
 
 const CatalogProductsStyled = styled.div`
@@ -103,7 +130,7 @@ const CatalogProductsStyled = styled.div`
   justify-items: center;
   overflow-y: scroll;
   margin-bottom: 220px;
-  
+
   ${menuAnimation}
 
   .card-container {
@@ -124,4 +151,4 @@ const CatalogProductsStyled = styled.div`
     z-index: 2;
   }
   ${ribbonAnimation}
-`
+`;
